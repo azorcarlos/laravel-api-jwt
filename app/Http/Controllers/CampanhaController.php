8f714da9d69b2d4512368d867cde9aff5eb5cd98
Azor <azorcarlos@gmail.com>
2019-12-12 12:09:18 -0300

    Commit parcial

--- app/Http/Controllers/CampanhaController.php
+++ app/Http/Controllers/CampanhaController.php
@@ -4,33 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Campanha;
+use App\Http\Requests\CampanhaRequest;
 
 class CampanhaController extends Controller
 {
-
     public function index()
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(CampanhaRequest $request)
     {
         $newCampanha = new Campanha();
         return $newCampanha->criarCampanhasGrupo($request);
     }
-
 
     public function show($id)
     {
         //
     }
 
-    public function update(Request $request, $id)
+    public function update(CampanhaRequest $request, $id)
     {
         $newCampanha = new Campanha();
         return $newCampanha->updateCampanhasGrupo($request,$id);
     }
-
 
     public function destroy($id)
     {
